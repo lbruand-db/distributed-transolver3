8f5e812757e651982267665febc16439653d32cf
lbruand-db <lucas.bruand+github@databricks.com>
2026-04-04 08:43:03 +0200

    Update copyright year to 2026 in all license headers and LICENSE file
    Co-authored-by: Isaac

--- transolver3/monitoring.py
+++ transolver3/monitoring.py
@@ -1,4 +1,4 @@
-# Copyright 2024 Databricks, Inc.
+# Copyright 2026 Databricks, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
